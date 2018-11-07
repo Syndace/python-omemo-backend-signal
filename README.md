9f3bb4e3a9bbf2401a9c64e9e3f83adb96fa9c56
Tim Henkes <tim.henkes@student.hs-rm.de>
2018-11-07 22:31:45 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+[![PyPI](https://img.shields.io/pypi/v/omemo-backend-signal.svg)](https://pypi.org/project/omemo-backend-signal/)
+[![PyPI - Python Version](https://img.shields.io/pypi/pyversions/omemo-backend-signal.svg)](https://pypi.org/project/omemo-backend-signal/)
+
+# python-omemo-backend-signal
+#### A Python implementation of the OMEMO Multi-End Message and Object Encryption protocol.
---
+++ setup.py
@@ -0,0 +1,54 @@
+from setuptools import setup, find_packages
+
+import os
+import sys
+
+sys.path.append(os.path.join(
+    os.path.dirname(os.path.abspath(__file__)),
+    "omemo_backend_signal"
+))
+
+import version
+
+with open("README.md") as f:
+    long_description = f.read()
+
+setup(
+    name = "omemo-backend-signal",
+    version = version.__version__,
+    description = (
+        "A Python implementation of the OMEMO Multi-End Message and Object Encryption " +
+        "protocol."
+    ),
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url = "https://github.com/Syndace/python-omemo-backend-signal",
+    author = "Tim Henkes",
+    author_email = "tim@cifg.io",
+    license = "GPLv3",
+    packages = find_packages(),
+    install_requires = [
+        "OMEMO>=0.8.0,<0.9"
+    ],
+    python_requires = ">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, <4",
+    zip_safe = True,
+    classifiers = [
+        "Development Status :: 4 - Beta",
+
+        "Intended Audience :: Developers",
+
+        "Topic :: Communications :: Chat",
+        "Topic :: Security :: Cryptography",
+
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7"
+    ]
+)
