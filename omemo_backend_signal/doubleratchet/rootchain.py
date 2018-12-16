37a074c4851cdff84b6753f2592aa5f87430cab1
Tim Henkes <tim.henkes@student.hs-rm.de>
2018-12-16 13:47:30 +0100

    Small bug fix in serialization

--- omemo_backend_signal/doubleratchet/rootchain.py
+++ omemo_backend_signal/doubleratchet/rootchain.py
@@ -3,7 +3,7 @@ from __future__ import absolute_import
 import doubleratchet
 
 class RootChain(doubleratchet.kdfchains.KDFChain):
-    def __init__(self, key):
+    def __init__(self, key = None):
         super(RootChain, self).__init__(
             doubleratchet.recommended.RootKeyKDF(
                 "SHA-256",
